d1c6611fa9367768838c40fc6ada1895030ebe8b
Arnab Ghosh <ghosharnab00@gmail.com>
2022-02-26 03:14:44 +0530

    Working on the Logic

--- app.js
+++ app.js
@@ -1,2 +1,31 @@
 var apiKey = "DI11MUVHJ3RMBP37UTOR";
-var url = "https://api.mailboxvalidator.com/v1/validation/single";
+var mainUrl = "https://api.mailboxvalidator.com/v1/validation/single";
+
+var emailInput = document.getElementById("input");
+var verifyClick = document.getElementById("input-button");
+var result = document.getElementById("is-verify");
+
+
+
+function getReq(url){
+
+return url + "?key=" + apiKey + "&email=" + emailInput.value
+
+}
+
+function verifyEmail(){
+    fetch (getReq(mainUrl), { mode: 'no-cors'})
+    .then ( response => response.json())
+    .then (json => {var validate = json.status;
+        function outPut_validate(){
+            if (validate == "True"){
+                result.innerHTML = "Valid";
+            }
+                else {
+                    result.innerHTML = "InValid"
+                }
+            }
+    }).catch(err => {console.log(err); alert("Server Limit Crossed , try with other API")})
+}
+
+verifyClick.addEventListener("click", verifyEmail);
