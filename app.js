a4559057f91a78621dc3720c898a878bd4be479b
Arnab Ghosh <ghosharnab00@gmail.com>
2022-03-11 02:26:45 +0530

    debugging

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var express = require('express');
-var app = express();
-var validator = require("email-validator");
+//import
+var express = require('express')
+var app = express()
+var validator = require("email-validator")
 
 var port = 3000;
 
+//Listne to port 3000
+app.listen(port, ()=> console.info(`Listning to port ${port}`));
+
+
+//Static Pages
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + '/public/css'))
+
+//view html
+app.get('', (req, res) => {
+    res.sendFile(__dirname + '/View/index.html')
+})
